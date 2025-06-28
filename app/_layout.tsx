2e6628532394fec981e17efe54eebae988cd71b1
Liberty apps <liberty.appsparis@gmail.com>
2025-06-28 11:57:45 +0200

    Updated package-lock.json

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,12 +2,14 @@ import { useEffect } from 'react';
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
 import { AuthProvider } from '../src/contexts/AuthContext';
-import * as SplashScreen from 'expo-splash-screen';
+import * as SplashScreen from 'expo-splash-screen'
+import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  useFrameworkReady();
   useEffect(() => {
     // Hide splash screen after layout is ready
     SplashScreen.hideAsync();
